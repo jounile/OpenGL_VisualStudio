8b11787e0846a07623ee191eeb72f8717d2c5cf7
Jouni Leino (ext) <jouni.leino@rightware.com>
2017-09-06 17:49:40 +0200

    Render texture to full window rectangle

--- main.cpp
+++ main.cpp
@@ -70,44 +70,32 @@ int main( )
 	GLfloat vertices[] =
     {
         // Rectangle - Full window
-		/*
-        // Positions          // Colors           // Texture Coords
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
-		*/
-		// Rectagle - Center window
-		/*
+
 		1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
 		1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
 		-1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
 		-1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
-		*/
-		// Triangle - Center window
-		- 0.5f, -0.5f, 0.0f,	1.0f, 0.0f, 0.0f,   0.5f, 0.5f,	// Top Center
-		0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,   0.5f, -0.5f, // Bottom Right
-		0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f,   0.0f, 0.5f, // Bottom Left
+		
     };
-	/*
+	
     GLuint indices[] =
     {  // Note that we start from 0!
         0, 1, 3, // First Triangle
         1, 2, 3  // Second Triangle
     };
-	*/
-	GLuint VBO, VAO;//, EBO;
+	
+	GLuint VBO, VAO, EBO;
     glGenVertexArrays( 1, &VAO );
     glGenBuffers( 1, &VBO );
-    //glGenBuffers( 1, &EBO );
+    glGenBuffers( 1, &EBO );
     
     glBindVertexArray( VAO );
     
     glBindBuffer( GL_ARRAY_BUFFER, VBO );
     glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
     
-    //glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
-    //glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
+    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
     
     // Position attribute
     glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * ) 0 );
@@ -164,8 +152,7 @@ int main( )
         
         // Draw container
         glBindVertexArray( VAO );
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-        //glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+        glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
         glBindVertexArray( 0 );
         
         // Swap the screen buffers
@@ -175,7 +162,7 @@ int main( )
     // Properly de-allocate all resources once they've outlived their purpose
     glDeleteVertexArrays( 1, &VAO );
     glDeleteBuffers( 1, &VBO );
-    //glDeleteBuffers( 1, &EBO );
+    glDeleteBuffers( 1, &EBO );
     
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate( );
